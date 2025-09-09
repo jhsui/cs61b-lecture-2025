b36d6e7a80898ab7825d974b76d55900e8541b79
jhsui <jiahansui1998@gmail.com>
2025-09-09 14:35:10 -0400

    after Lecture 4 - Lists II: SLLists

--- src/SLList.java
+++ src/SLList.java
@@ -13,72 +13,117 @@ public class SLList {
     }
 
 
-    private IntNode first;
+    // dummy node
+    // To avoid the special case???
+    private IntNode sentinel;
+    private int size;
 
     // Constructor
     public SLList(int x) {
-        first = new IntNode(x, null);
+        sentinel = new IntNode(83102743, null);
+        sentinel.next = new IntNode(x, null);
+        size = 1;
+    }
+
+    public SLList() {
+        sentinel = new IntNode(83102743, null);
+        size = 0;
     }
 
     public void addFirst(int x) {
-        first = new IntNode(x, first);
+
+        sentinel.next = new IntNode(x, sentinel.next);
+        size++;
     }
 
-    public int getFirst() {
-        return first.item;
+    public int getSentinel() {
+        return sentinel.next.item;
     }
+
+//    public int size() {
+//        int i = 0;
+//
+//        IntNode node = this.first;
+//
+//        while (true) {
+//            if (node == null) {
+//                break;
+//            } else {
+//                node = node.next;
+//                i++;
+//            }
+//        }
+//
+//        return i;
+//    }
 
     public int size() {
-        int i = 0;
-
-        IntNode node = this.first;
-
-        while (true) {
-            if (node == null) {
-                break;
-            } else {
-                node = node.next;
-                i++;
-            }
-        }
-
-        return i;
+        return size;
     }
 
 
-    // add an IntNode at the end of the list.
+//    private int size(IntNode p) {
+//        if (p == null) {
+//            return 0;
+//        }
+//
+//        return 1 + size(p.next);
+//    }
+
+
+//     add an IntNode at the end of the list.
+//    public void addLast(int x) {
+//
+//        int size = this.size();
+//
+//        // 既然我们只能在最前面添加node，那么干脆从最后一个开始加起，一直加到原first的第一个
+//        IntNode newFirst = new IntNode(x, null);
+//
+//
+//        //找到最后的node，并得到它的item
+//        while (size > 0) {
+//            IntNode node = this.first;
+//            int i = 0;
+//            while (true) {
+//                if (node.next == null) {
+//                    newFirst = new IntNode(node.item, newFirst);
+//                    break;
+//                } else {
+//                    node = node.next;
+//                    i++;
+//                }
+//            }
+//            size--;
+//
+//        }
+//        this.first = newFirst;
+//
+//    }
+
     public void addLast(int x) {
 
-        int size = this.size();
+        IntNode p = sentinel;
 
-        // 既然我们只能在最前面添加node，那么干脆从最后一个开始加起，一直加到原first的第一个
-        IntNode newFirst = new IntNode(x, null);
-
-
-        //找到最后的node，并得到它的item
-        while (size > 0) {
-            IntNode node = this.first;
-            int i = 0;
-            while (true) {
-                if (node.next == null) {
-                    newFirst = new IntNode(node.item, newFirst);
-                    break;
-                } else {
-                    node = node.next;
-                    i++;
-                }
-            }
-            size--;
-
+        while (p.next != null) {
+            p = p.next;
         }
-        this.first = newFirst;
 
+        p.next = new IntNode(x, null);
+
+        size++;
+        // the reason why i didn't think of this way is that i thought if i did so, there would be a p from nowhere.
+        // instead, the p is still the last of the first.
     }
 
     public static void main(String[] args) {
-        SLList L = new SLList(15);
+        SLList LL = new SLList(15);
+        SLList L = new SLList();
         L.addFirst(10);
         L.addFirst(5);
+        LL.addFirst(10);
+        LL.addFirst(5);
+
+        System.out.println(L.size());
 
         L.addLast(99);
 
